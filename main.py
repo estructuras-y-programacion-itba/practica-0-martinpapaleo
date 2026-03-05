9c219fee2833c3bb08a9af9441d7fc753ef4d08f
martinpapaleo <martinpapaleo1@gmail.com>
2026-03-05 18:11:43 -0300

    changes

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
 # Tu implementacion va aqui
-def hola_mundo():
-    return "hola_mundo"
+import random
+import pandas as pd
+
+puntos = pd.read_csv('generala_scoreboard.csv', index_col=0)
+
+def tirar_dado():
+    # simula un tiro de dado
+    return random.randint(1,6)
+
+def volcar(nro: int) -> list:
+    sol = []
+    for i in range(nro):
+        sol.append(tirar_dado())
+    return sol
+
+def turno():
+    return
+    
+def juego():
+    return
 
 
 def main():
-    # Aqui ejecutas tus soluciones
-    print(hola_mundo())
+    tiro = volcar(2)
+    tiro.sort()
+    print(tiro)
+    
 
 
 # No cambiar a partir de aqui
 if __name__ == "__main__":
     main()
-
-print('  ')
