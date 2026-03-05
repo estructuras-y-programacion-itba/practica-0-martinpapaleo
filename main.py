cbc1c87c17b762b566577ea6a8d0b3c77aec6ec6
martinpapaleo <martinpapaleo1@gmail.com>
2026-03-05 18:55:55 -0300

    changes

--- main.py
+++ main.py
@@ -2,7 +2,10 @@
 import random
 import pandas as pd
 
-puntos = pd.read_csv('generala_scoreboard.csv', index_col=0)
+def puntos_anotados():
+    # lee y retorna tabla para anotar puntos
+    puntos = pd.read_csv('generala_scoreboard.csv', index_col=0)
+    return puntos
 
 def tirar_dado():
     # simula un tiro de dado
@@ -14,17 +17,39 @@ def volcar(nro: int) -> list:
         sol.append(tirar_dado())
     return sol
 
+def tabla_config(datos):
+    
+    return datos
+
+def cant_jug() -> int:
+    while True:
+        try:
+            cant = int(input('Ingrese cantidad de jugadores (1,2,3 o 4): '))
+            if cant in range(5):
+                return cant
+            else:
+                raise ValueError
+        except ValueError:
+            print('Respete las instrucciones previas!')
+
+'''
 def turno():
     return
-    
+    h
 def juego():
     return
-
+'''
 
 def main():
-    tiro = volcar(2)
-    tiro.sort()
-    print(tiro)
+    jugadores = cant_jug()
+    if not jugadores:
+        print('Nadie quiere jugar :(')
+    else:
+        tiro = volcar(2)
+        tiro.sort()
+        puntos = puntos_anotados()
+        print(f'Cantidad de jugadores: {jugadores}\n')
+        print('Tabla de Puntos: '+ '\n'*2 + f'{puntos}')
     
 
 
